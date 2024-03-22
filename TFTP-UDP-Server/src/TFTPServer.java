2e95a0b92b3937ba5eecce31ffc79ebb01a7776a
fiery <johncw004@gmail.com>
2024-03-22 19:25:59 +0000

    added complete read request functionality to server side, not the cleanest though haha

--- TFTP-UDP-Server/src/TFTPServer.java
+++ TFTP-UDP-Server/src/TFTPServer.java
@@ -3,6 +3,8 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
@@ -11,10 +13,12 @@ public class TFTPServer {
 
     private DatagramSocket socket;
     private static final byte[] octetBytes = "octet".getBytes();
-    private static final int bufSize = 256;
+    private static final int bufSize = 516;
     private byte[] buf = new byte[bufSize];
-    private byte[] fileBuf;
+
     private static final String fileDirectory = "./../Files";
+    private byte opcode;
+    private String currentFilename;
 
     public TFTPServer(DatagramSocket socket) {
         this.socket = socket;
@@ -27,7 +31,7 @@ public class TFTPServer {
      *  opcode  | filename | 0      | Mode   | 0
      */
     private byte[] handleRequest(DatagramPacket packet, byte opcode) {
-        System.out.println(opcode == 1 ? "RRQ" : "WRQ");
+
         byte[] data = packet.getData();
         int filenameSize = 0;
         for (int i = 2; i < data.length-2; i++) {
@@ -37,12 +41,12 @@ public class TFTPServer {
             }
         }
         byte[] filenameBytes = new byte[filenameSize];
-        for (int i = 2; i < filenameSize + 2; i++) {
-            filenameBytes[i-2] = data[i];
-        }
+        System.arraycopy(data, 2, filenameBytes, 0, filenameSize + 2 - 2);
         String filename = new String( filenameBytes, 0);
+        System.out.println((opcode == 1 ? "Read request" : "Write request")+ " received for "+filename);
+        currentFilename = filename;
         if (opcode == 1){
-            return(getData(filename, 0));
+            return(getDataPacket(filename, 1));
         } else {
             return getAck(packet.getAddress(), packet.getPort(), (byte) 0);
         }
@@ -54,9 +58,34 @@ public class TFTPServer {
      * ---------|----------|---------|
      *  opcode  | BlockNum | Data
      */
-    private byte[] getData(String filename, int blockNumber) {
-        System.out.println("Reading: "+filename);
-        return new byte[bufSize];
+    private byte[] getDataPacket(String filename, int blockNumber) {
+        byte[] fileData;
+
+        try {
+            fileData = Files.readAllBytes(Paths.get("src/" + filename));
+            if (blockNumber == ((fileData.length / 512) + 2)){ // ACK call was acknowledging last packet in file
+                return null;
+            }
+            int to;
+            if (blockNumber > fileData.length / 512) {
+                to = (512 * (blockNumber - 1)) + (fileData.length % 512);
+            } else {
+                to = 512 * blockNumber;
+            }
+            byte[] segment = Arrays.copyOfRange(fileData, (blockNumber-1) * 512, to);
+            byte[] dataPacket = new byte[4+segment.length];
+            dataPacket[0] = 0;
+            dataPacket[1] = 3;
+            dataPacket[2] = 0;
+            dataPacket[3] = (byte) blockNumber;
+
+            System.arraycopy(segment, 0, dataPacket, 4, segment.length);
+            return dataPacket;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
     /*
      * ACK Packet
@@ -74,29 +103,16 @@ public class TFTPServer {
     private byte[] handleDataPacket(DatagramPacket packet) {
         return null;
     }
-    private byte[] handleAck(DatagramPacket packet) {
+    private byte[] handleAck(int blockNumber) {
         return null;
     }
     private byte[] handleError(DatagramPacket packet) {
         return null;
     }
-    private byte[] getResponseHeader(DatagramPacket packet) {
-        byte opcode = packet.getData()[1];
-        switch (opcode) {
-            case 1:
-            case 2:
-                return(handleRequest(packet, opcode));
-            case 3:
-                return(handleDataPacket(packet));
-            case 4:
-                return(handleAck(packet));
-            case 5:
-                return(handleError(packet));
-            default:
-                System.out.println("incorrect opcode received");
-                return null;
-        }
+    private int getBlockNumber(byte[] packetData) {
+        return (((packetData[2] & 0xff) << 8) | (packetData[3] & 0xff));
     }
+
     public void receiveAndSend() {
         System.out.println("TFTP server running");
         while (true) {
@@ -104,23 +120,42 @@ public class TFTPServer {
                 buf = new byte[bufSize];
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
                 socket.receive(packet);
-                InetAddress clientAddr = packet.getAddress();
-
-                //blocking annoying packets on home network
-                // TODO: Remember to remove lol
-                if (Objects.equals(clientAddr.toString(), "/192.168.0.57")){
+                byte[] data = packet.getData();
+                opcode = data[1];
+                if (opcode > 5 || opcode < 1){
                     continue;
                 }
-
+                InetAddress clientAddr = packet.getAddress();
                 int clientPort = packet.getPort();
                 System.out.println("packet received from "+clientAddr+":"+clientPort);
-                //System.out.println(Arrays.toString(data));
-                //System.out.println(opcode);
-
                 String message = new String(packet.getData(), 0, packet.getLength());
                 System.out.println("message received: "+message);
 
-                buf = getResponseHeader(packet);
+                //System.out.println(Arrays.toString(data));
+                //System.out.println(opcode);
+
+                switch (opcode) {
+                    case 1: // Read Request
+                    case 2: // Write Request
+                        buf = handleRequest(packet, opcode);
+                        break;
+                    case 3: // Data
+                    case 4: // Acknowledgement
+                        int blockNumber = getBlockNumber(data);
+                        System.out.println("blocknumber: "+blockNumber);
+                        //buf = handleAck(blockNumber);
+                        if (blockNumber > 0) {
+                            buf = getDataPacket(currentFilename, blockNumber + 1);
+                        }
+                    case 5: // Error
+                        break;
+                    default:
+                        System.out.println("incorrect opcode received");
+                        break;
+                }
+                if (buf == null) {
+                    continue;
+                }
                 packet = new DatagramPacket(buf, buf.length, clientAddr, clientPort);
                 socket.send(packet);
                 System.out.println("Replied to client");
